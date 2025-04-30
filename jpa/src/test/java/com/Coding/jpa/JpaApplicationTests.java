0d82ceb45a4ee6fdf580fafecea5e3287f7dc7a9
ILYESS BASSOUMI <156471600+bassoumi@users.noreply.github.com>
2025-04-30 19:51:18 +0100

    second project

---
+++ jpa/src/test/java/com/Coding/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Coding.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
